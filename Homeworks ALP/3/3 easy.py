27fb58380d56a0b5eabda89412381d2a781bf1dc
Mysterao <125818806+Mysterao@users.noreply.github.com>
2025-09-21 22:25:40 +0200

    .

--- Homeworks ALP/3/3 easy.py
+++ Homeworks ALP/3/3 easy.py
@@ -1,6 +1,7 @@
-
+import random
 list = [11, 13, 25, 40, 17, 35, 7, 100, 5, 2, 40, 75,
         73, 125, 20, 43, 41, 53, 55, 59]
+list = [2, 7, 9, 13, 11, 20]
 
 def prime_number_tester(list):
     nonprime = []
@@ -20,7 +21,6 @@ def prime_number_tester(list):
                 divisible = True
                 nonprime.append(list[i])
             j += 1
-    print(upto)
     if nonprime == None:
         return 1
     else:
@@ -45,6 +45,11 @@ def divisors(element):
 #     n = int(input())
 #     list.append(n)
 
+# for i in range(20):
+#     n = random.randint(0, 300)
+#     list.append(n)
+# print(list)
+
 list = prime_number_tester(list)
 if list != 1:
     print(list)
@@ -57,13 +62,29 @@ if list != 1:
         if len(divisors(list[i])) >= max_length:
             index_max_length = i
             max_length = len(list_of_divisors)
+    # print(matrix)
+    # print(index_max_length)
+    final_divisors = []
     for i in range(len(matrix[index_max_length])):
-        number = matrix[index_max_length]
+        number = matrix[index_max_length][i]
         in_all_lists = True
         for j in range(len(matrix)):
-            if index_max_length != i:
-                if matrix[index_max_length][i] not in matrix[j]:
+            if index_max_length != j:
+                if number not in matrix[j]:
                     in_all_lists = False
         if in_all_lists == True:
             for j in range(len(matrix)):
-                if index_max_length 
+                if index_max_length != j:
+                    if number in matrix[j]:
+                        matrix[j].pop(matrix[j].index(number))
+                        matrix[j].append(0)
+        if in_all_lists == True:
+            final_divisors.append(number)
+    # print(final_divisors)
+final_number = 1
+if len(final_divisors) == 1:
+    final_number += -1 + final_divisors[0]
+if len(final_divisors) > 1:
+    for i in range(len(final_divisors)):
+        final_number *= final_divisors[i]
+print(final_number)
