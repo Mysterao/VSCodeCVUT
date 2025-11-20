24bbc7eee9a9b3a2a1a5dab814d02420bd1cb8ec
Mysterao <125818806+Mysterao@users.noreply.github.com>
2025-11-20 10:08:30 +0100

    .

---
+++ 8.1/8/test.py
@@ -0,0 +1,19 @@
+for i in range(-1, 2):
+    print(i)
+
+# def detect_triple(playerfield):
+#     counter = 0
+#     coords = []
+#     first_color = ""
+#     ranks = {"-1" : "yellow", "1" : "yellow", "-2" : "blue", "2" : "blue"}
+#     #vertical
+#     i = 0
+#     while i != len(playerfield) and counter != 3:
+#         j = 0
+#         while j != len(playerfield[i]) and counter != 3:
+#             if playerfield[i][j] != "0":
+#                 counter += 1
+#                 coords.append((i, j))
+#                 first_color = playerfield[]
+#             j += 1
+#         i += 1
